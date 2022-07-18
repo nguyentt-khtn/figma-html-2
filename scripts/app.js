196e7b95d6da295013a9f1cc88bc69799bc1fc56
nguyentt-khtn <ttnguyenkhtn@gmail.com>
2022-07-18 20:53:18 +0700

    update responsive

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,19 @@
 $('.testimonial-list').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow: false,
-    nextArrow: false,
-    dots: true,
-  });
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: false,
+  nextArrow: false,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    }
+  ],
+});
